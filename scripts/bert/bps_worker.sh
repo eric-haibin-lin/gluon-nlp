79b778c13bff1b5438373fea66f3ccf2710e1175
Haibin Lin <linhaibin.eric@gmail.com>
2019-09-30 21:52:51 -0700

    fix arg

--- scripts/bert/bps_worker.sh
+++ scripts/bert/bps_worker.sh
@@ -40,5 +40,5 @@ if [ $DMLC_ROLE = 'worker' ]; then
        --lr 1e-4 --total_batch_size $BS --accumulate 1 --raw --short_seq_prob 0 --log_interval 10 \
        --max_seq_length 512 \
        --eval_use_npz --synthetic_data \
-       --accumulate 1 --model $MODEL --total_batch_size_eval $BS --backend byteps 2>&1 | tee -a result.log
+       --accumulate 1 --model $MODEL --total_batch_size_eval $BS --comm_backend byteps 2>&1 | tee -a result.log
 fi
